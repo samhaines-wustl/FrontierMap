79ee3aba80872ffb8891c61ed82c7ae5dec5871c
Sam <78055941+samhaines-wustl@users.noreply.github.com>
2025-10-03 19:20:40 -0500

    Added python script to generate manifest file

---
+++ gen_manifest.py
@@ -0,0 +1,13 @@
+# import OS module
+import os
+import json
+# Get the list of all files and directories
+path = "./locations"
+dir_list = os.listdir(path)
+print("Files and directories in '", path, "' :")
+# prints all files
+cleaned_list = [file for file in dir_list if '.json' in file]
+print(cleaned_list)
+
+with open("./json/manifest.json", 'w') as outfile:
+    json.dump(cleaned_list, outfile, indent=2)
